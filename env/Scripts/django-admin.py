a8b3d2994c3317dfa548eca169bd498d8118f050
Martin Westermeyer <martinwestermeyer@gmail.com>
2019-12-29 15:56:15 -0300

    barebones workspace

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\marti\OneDrive\Documents\Proyectos\webwester\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
