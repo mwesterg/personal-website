173cb8cbe169b1e14b9e26434a7ef83dbf3a0b6c
Martin Westermeyer <MartinWestermeyer@gmail.com>
2019-12-30 10:47:57 -0300

    working homepage

--- webwester/urls.py
+++ webwester/urls.py
@@ -19,3 +19,21 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# Use include() to add paths from the catalog application 
+from django.urls import include
+from django.urls import path
+
+urlpatterns += [
+    path('main/', include('main.urls')),
+]
+
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='main/', permanent=True)),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT);
